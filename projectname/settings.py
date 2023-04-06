6e5aa4eceb30221a454ce80f909371d7bb3c9f7c
Sripriya Simhadri <simhadrisripriya@Sripriyas-MacBook-Air.local>
2023-04-06 01:49:22 -0400

    vercel

--- projectname/settings.py
+++ projectname/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e9p7y&j44qmh2p^e^erq2-vcz0m*z=!r@qljzn8ztg&b4spj&+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
